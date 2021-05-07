ca3bc92df65cfbd015d5a2fab22ce996c617bf1e
asuka-mizu <82270539+asuka-mizu@users.noreply.github.com>
2021-05-08 04:24:08 +0900

    Update main.js
    correct the  two mistakes

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
+    if(mySrc === 'images/peace-symbol.png') {
       myImage.setAttribute ('src','images/firefox2.png');
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
+      myImage.setAttribute ('src','images/peace-symbol.png');
     }
 }
