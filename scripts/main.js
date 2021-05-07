457f3d3057fcd442843a470cef702da03df3d650
asuka-mizu <82270539+asuka-mizu@users.noreply.github.com>
2021-05-08 04:32:27 +0900

    Update main.js
    change firefox2.png to emoji.png

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/peace-symbol.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+      myImage.setAttribute ('src','images/emoji.png');
     } else {
       myImage.setAttribute ('src','images/peace-symbol.png');
     }
